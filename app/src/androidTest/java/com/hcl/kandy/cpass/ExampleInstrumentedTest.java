3953ce7b21506883ced0a4a22a3983af4c3d1720
ashishhcl07 <45618037+ashishhcl07@users.noreply.github.com>
2021-11-10 19:28:50 +0530

    Update ExampleInstrumentedTest.java
    Addig login test case

--- app/src/androidTest/java/com/hcl/kandy/cpass/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hcl/kandy/cpass/ExampleInstrumentedTest.java
@@ -1,13 +1,28 @@
 package com.hcl.kandy.cpass;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 import android.content.Context;
+import android.provider.ContactsContract;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import com.hcl.kandy.cpass.activities.LoginActivity;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +31,34 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    @Rule
+    public ActivityScenarioRule<LoginActivity> activityRule = new ActivityScenarioRule<>(LoginActivity.class);
+    private String url = "abcd";
 
-        assertEquals("com.hcl.kandy.cpass", appContext.getPackageName());
+    @Test
+    public void verifyMessageSentToMessageActivity() {
+
+        // Types a message into a EditText element.
+        onView(withId(R.id.et_url))
+                .perform(typeText(url), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name))
+                .perform(typeText(url), closeSoftKeyboard());
+        onView(withId(R.id.et_user_password))
+                .perform(typeText(url), closeSoftKeyboard());
+        onView(withId(R.id.et_user_client))
+                .perform(typeText(url), closeSoftKeyboard());
+
+        // Clicks a button to send the message to another
+        // activity through an explicit intent.
+        onView(withId(R.id.button_login)).perform(click());
+
+        // Verifies that the DisplayMessageActivity received an intent
+        // with the correct package name and message.
+       /* intended(allOf(
+                hasComponent(hasShortClassName(".DisplayMessageActivity")),
+                toPackage(PACKAGE_NAME),
+                hasExtra(MainActivity.EXTRA_MESSAGE, MESSAGE)));*/
+
     }
+
 }
